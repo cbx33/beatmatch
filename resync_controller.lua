049b34233f2b156b3563f59668bdc53b2efaaab6
Pete Savage <silentkeystroke@googlemail.com>
2023-01-19 19:39:25 +0000

    Update resync_controller.lua

--- resync_controller.lua
+++ resync_controller.lua
@@ -1,16 +1,21 @@
-old_tempo = 0
+last_future_add = 0
 reaper.gmem_attach("resync2")
 
 function main()
    -- reaper.ClearConsole()
    tempo = tonumber(string.format("%.1f", reaper.gmem_read(0)))
+   fut_tempo = tonumber(string.format("%.1f", reaper.gmem_read(1)))
    if (tempo > 10) and (reaper.GetPlayState() == 1)then
-     if (old_tempo ~= tempo) then
-       reaper.ShowConsoleMsg("not equal\n")
-       reaper.SetTempoTimeSigMarker(0, -1, reaper.GetPlayPosition(), -1, -1, tempo, 0, 0, false)
+     if (first_run ~= 1) and (reaper.GetPlayPosition() < last_future_add) then
+       reaper.ShowConsoleMsg("deleting old tempo\n")
+       arb = reaper.FindTempoTimeSigMarker(0, reaper.gmem_read(2) + 1)
+       reaper.DeleteTempoTimeSigMarker(0, arb)
+     end
+     reaper.ShowConsoleMsg("setting tempo\n")
+     reaper.SetTempoTimeSigMarker(0, -1, reaper.GetPlayPosition(), -1, -1, tempo, 0, 0, false)
+     reaper.SetTempoTimeSigMarker(0, -1, reaper.gmem_read(2), -1, -1, fut_tempo, 0, 0, false)
        -- reaper.SetCurrentBPM(0, tempo, 1)
-       old_tempo = tempo
-    end
+     reaper.gmem_write(0, 0)
    end
    reaper.defer(main)
 end
